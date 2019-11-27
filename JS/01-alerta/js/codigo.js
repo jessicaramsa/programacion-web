128c26ee1552793eb78fc6546b156ad22efe3166
Jéssica Ramírez <jessicaramsa@gmail.com>
2019-11-27 16:02:51 -0600

    js

---
+++ JS/01-alerta/js/codigo.js
@@ -0,0 +1,4 @@
+// mensaje alerta inicial de prueba
+alert("Un mensaje de prueba");
+// mensaje de alerta
+alert("Soy el primer script");
---
+++ JS/05-usuariovariable/codigo.js
@@ -0,0 +1,2 @@
+let user = 'Jéssica';
+alert('Hola mundo\nque fácil es incluir \'comillas simples\'\ny "comillas dobles"');
